11ff0298aa33dc66071e33d5da720ac1e73f6399
maxrexfax <maxrexfax@gmail.com>
2021-05-05 00:28:21 +0300

    final daily

--- app/src/main/java/com/example/criminalintent/Crime.java
+++ app/src/main/java/com/example/criminalintent/Crime.java
@@ -1,10 +1,19 @@
 package com.example.criminalintent;
 
+import java.util.Date;
 import java.util.UUID;
 
 public class Crime {
     private UUID mId;
     private String mTitle;
+    private Date mDate;
+    private boolean mSolved;
+
+    public Crime() {
+        // Генерирование уникального идентификатора
+        mId = UUID.randomUUID();
+        mDate = new Date();
+    }
 
     public UUID getmId() {
         return mId;
@@ -22,8 +31,20 @@ public class Crime {
         this.mTitle = mTitle;
     }
 
-    public Crime() {
-        // Генерирование уникального идентификатора
-        mId = UUID.randomUUID();
+    public Date getDate() {
+        return mDate;
     }
+
+    public void setDate(Date mDate) {
+        this.mDate = mDate;
+    }
+
+    public boolean isSolved() {
+        return mSolved;
+    }
+
+    public void setSolved(boolean mSolved) {
+        this.mSolved = mSolved;
+    }
+
 }
--- app/src/main/java/com/example/criminalintent/MainActivity.java
+++ app/src/main/java/com/example/criminalintent/MainActivity.java
@@ -1,14 +1,24 @@
 package com.example.criminalintent;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_crime);
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragmentContainer);
+
+        if (fragment == null) {
+            fragment = new CrimeFragment();
+            fm.beginTransaction().add(R.id.fragmentContainer, fragment).commit();
+        }
     }
 }
